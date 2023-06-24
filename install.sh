d60d10d0d50194b0cc3d18ea0b92ff177ddfd8b2
Safely1429 <137442705+Safely1429@users.noreply.github.com>
2023-06-24 16:06:01 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,18 +14,18 @@ apt update
 apt upgrade -y
 
 # Making .config and Moving config files and background to Pictures
-cd $builddir
-mkdir -p /home/$username/.config
-mkdir -p /home/$username/Pictures
-mkdir -p /home/$username/Pictures/backgrounds
-cp -R dotconfig/* /home/$username/.config/
-mv user-dirs.dirs /home/$username/.config
-chown -R $username:$username /home/$username
+#cd $builddir
+#mkdir -p /home/$username/.config
+#mkdir -p /home/$username/Pictures
+#mkdir -p /home/$username/Pictures/backgrounds
+#cp -R dotconfig/* /home/$username/.config/
+#mv user-dirs.dirs /home/$username/.config
+#chown -R $username:$username /home/$username
 
 # Installing Essential Programs 
 apt install feh kitty rofi picom lxpolkit x11-xserver-utils unzip pulseaudio pavucontrol build-essential -y
 # Installing Other less important Programs
-apt install flameshot psmisc vim lxappearance lightdm -y
+apt install psmisc vim lxappearance lightdm -y
 
 # Enable graphical login and change target from CLI to GUI
 systemctl enable lightdm
